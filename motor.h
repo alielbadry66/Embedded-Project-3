870d4d149d2148b9be668aff3ebb85b221dd9949
Omarwahba611 <114486478+Omarwahba611@users.noreply.github.com>
2023-12-07 01:56:39 +0200

    Update motor.h

--- motor.h
+++ motor.h
@@ -1,14 +1,9 @@
 #ifndef MOTOR_H_
 #define MOTOR_H_
 
-// Define the enumeration for motor direction
-typedef enum {
-    DIRECTION_CW,
-    DIRECTION_CCW
-} tenuDirection;
-
 void DC_Init();
-void DC_Start(unsigned char MotorID, tenuDirection Direction, unsigned char Speed);
+void DC_Start_CW(unsigned char MotorID, unsigned char Speed);
+void DC_Start_CCW(unsigned char MotorID, unsigned char Speed);
 void DC_Stop(unsigned char MotorID);
 
 #endif
