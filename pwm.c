e42819f3ceb92227f8794448ffabd2c91a6c72b4
Omarwahba611 <114486478+Omarwahba611@users.noreply.github.com>
2023-12-07 01:57:02 +0200

    Update pwm.c

--- pwm.c
+++ pwm.c
@@ -4,7 +4,7 @@
 void PWM_init(){
     DDRD |= (1 << PD6); // Fast PWM output at OC0A pin
 
-    TCCR0A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00); // Non-Inverting Fast PWM mode 3 using OCR A unit
+    TCCR0A |= (1 << COM0A1) | (1 << WGM01) | (1 << WGM00); // Non-Inverting Fast PWM mode 3 using OCR A
     TCCR0B |= (1 << CS00); // No-Prescalar
 
     Set_DutyCycle(0);
