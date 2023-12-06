2c0ff62f1cd87637d401c35e2d3cc69d0545ed7a
Omarwahba611 <114486478+Omarwahba611@users.noreply.github.com>
2023-12-07 01:56:22 +0200

    Update motor.c

--- motor.c
+++ motor.c
@@ -1,29 +1,36 @@
 #include "motor.h"
 #include <avr/io.h>
+#include "pwm.h"
 
 #define PWM_PIN 0 // Define PWM PIN 
-#define DIR_PIN 3 // Define DIR PIN
+#define DIR_PIN 1 // Define DIR PIN
 
-unsigned char MotorArray[][2] = {
-    {PWM_PIN, DIR_PIN} // Motor 0: {PWM_pin, Direction_pin}
-};
+// Define MotorArray 
+unsigned char MyMotorArray[1][2];
 
 void DC_Init() {
-    // Set the motor pins as outputs
-    DDRD |= (1 << MotorArray[0][PWM_PIN]) | (1 << MotorArray[0][DIR_PIN]);
+    MyMotorArray[0][0] = PWM_PIN;
+    MyMotorArray[0][1] = DIR_PIN;
 
+    // Set the motor pins as outputs
+    DDRD |=  (1 << MyMotorArray[0][PWM_PIN]);
+    DDRD |=  (1 << MyMotorArray[0][DIR_PIN]);
     // Initialize the PWM module
     PWM_init();
 }
 
-void DC_Start(unsigned char MotorID, tenuDirection Direction, unsigned char Speed) {
-    // Set the direction pin
-    if (Direction == DIRECTION_CW) {
-        PORTD |= (1 << MotorArray[MotorID][DIR_PIN]);
-    } else {
-        PORTD &= ~(1 << MotorArray[MotorID][DIR_PIN]);
-    }
+void DC_Start_CW(unsigned char MotorID, unsigned char Speed) {
+    // Set the direction pin for clockwise movement
+    PORTD |= (1 << MyMotorArray[MotorID][DIR_PIN]);
+    
+    // Start the motor by applying PWM and define speed
+    Set_DutyCycle(Speed);
+}
 
+void DC_Start_CCW(unsigned char MotorID, unsigned char Speed) {
+    // Set the direction pin for anti-clockwise movement
+    PORTD &= ~(1 << MyMotorArray[MotorID][DIR_PIN]);
+    
     // Start the motor by applying PWM and define speed
     Set_DutyCycle(Speed);
 }
@@ -31,5 +38,5 @@ void DC_Start(unsigned char MotorID, tenuDirection Direction, unsigned char Spee
 void DC_Stop(unsigned char MotorID) {
     // Stopping the motor by turning off the PWM and direction pins
     Set_DutyCycle(0);
-    PORTD &= ~(1 << MotorArray[MotorID][DIR_PIN]);
+    PORTD &= ~(1 << MyMotorArray[MotorID][DIR_PIN]);
 }
