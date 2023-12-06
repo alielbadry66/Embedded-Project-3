729b1cd74f21a2b2dc26a1d0bb83ea5523b1a72b
Omarwahba611 <114486478+Omarwahba611@users.noreply.github.com>
2023-12-07 01:57:28 +0200

    Update temp.c

--- temp.c
+++ temp.c
@@ -1,51 +1,47 @@
+#include <avr/io.h>
+#include <stdint.h>
 #include "temp.h"
-#include "LCD.h"
-#include "Keypad.h"
-#include "motor.h"
-#include "i2c.h"
 
+// Define constants
+#define TEMPERATURE_SENSOR_PIN 0   // Analog pin A0 on Arduino Uno
 
-#define I2C_TEMPERATURE_REGISTER 0x00 // Omar Replace this with the actual register address for temperature
+// Function to initialize the ADC
+void initADC() {
+    // Set reference voltage to AVCC and left-adjust result
+    ADMUX = (1 << REFS0) | (1 << ADLAR);
 
-float readTemperature() {
-    // Start I2C communication
-    i2cStart();
+    // Enable ADC, enable ADC interrupt, and set prescaler to 128
+    ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+}
 
-    // Select the I2C device and write the register address
-    i2cWrite((I2C_DEVICE_ADDRESS << 1) | I2C_WRITE);
-    i2cWrite(I2C_TEMPERATURE_REGISTER);
-
-    // Select the I2C device and read the temperature
-    i2cWrite((I2C_DEVICE_ADDRESS << 1) | I2C_READ);
+// Function to read the temperature
+uint16_t readADC() {
+    // Select the ADC channel
+    ADMUX = (ADMUX & 0xF8) | (TEMPERATURE_SENSOR_PIN & 0x07);
     
-    // Read temperature MSB (Most Significant Byte)
-    uint8_t msb = i2cReadACK();
-
-    // Read temperature LSB (Least Significant Byte)
-    uint8_t lsb = i2cReadNACK();
-
-    // Stop I2C communication
-    i2cStop();
-
-    // Combine MSB and LSB to get the temperature value
-    int16_t temperatureRaw = (msb << 8) | lsb;
-
-    // Convert the raw temperature value to a floating-point value (abdelrahman adjust this according to the sensor's data format from the data sheet)
-    float temperature = temperatureRaw / 256.0;
-
-    return temperature;
+    // Start the conversion
+    ADCSRA |= (1 << ADSC);
+    
+    // Wait for the conversion to complete
+    while (ADCSRA & (1 << ADSC));
+    
+    // Return the ADC result
+    return ADC;
 }
 
-
-float getTemperatureInput() {
-    char key;
-    do {
-        key = Keypad_ReadKey(); // Keep reading until a valid key is pressed
-    } while (key == '\0');
-
-    return key - '0'; // Convert char to int (assuming single-digit input)
+// Function to initialize the temperature sensor
+void initTemperatureSensor() {
+    // Initialize the ADC
+    initADC();
 }
 
-void displayParameters(float temperature) {
-    updateLCD(temperature, 0.0); // Assuming the upper limit is not used here until we decide the final application
+// Function to read the temperature from the LM35 sensor
+float readTemperature() {
+    // Read the temperature from the LM35
+    uint16_t adcValue = readADC();
+    
+    // Convert ADC value to temperature in Celsius
+    float temperatureC = (adcValue * 0.48876) - 50.0;  // LM35 provides 10mV per degree Celsius
+    
+    return temperatureC;
 }
